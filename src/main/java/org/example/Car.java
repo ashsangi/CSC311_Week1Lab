0ecfa3c953f2dbe437ba4358ccda979cc365679a
Ashraf Sangi <ashraf.sangi27@gmail.com>
2024-08-29 22:04:56 -0400

    finished vehicle class

--- src/main/java/org/example/Car.java
+++ src/main/java/org/example/Car.java
@@ -1,4 +1,33 @@
 package org.example;
 
-public class Car {
+public class Car extends Vehicle{
+    String brand;
+    public Car(){
+        numberOfWheels = 4;
+        color = "white";
+        engineSize = 3;
+        fuelType = "gasoline";
+        brand = "Honda";
+    }
+    public Car(int wheels, String color, float engineSize, String fueltype, String brand){
+        this.numberOfWheels = wheels;
+        this.color = color;
+        this.engineSize = engineSize;
+        this.fuelType = fueltype;
+        this.brand = brand;
+    }
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void honk(){
+        System.out.println("Honk, honk!");
+    }
+    public void displayInfo(){
+        System.out.println("Wheels: " + getNumberOfWheels() + ", Color: " + getColor() + ", Engine Size: " + getEngineSize() + ", Fuel Type: " + getFuelType() + ", Brand: " + getBrand());
+    }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,9 @@
 package org.example;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args){
+        Car c1 = new Car();
+        c1.displayInfo();
     }
+
 }
