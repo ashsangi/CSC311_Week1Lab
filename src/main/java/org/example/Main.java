524f840c7629f7486282006d664d7507d73a6fa8
sanga31 <sanga31@farmingdale.edu>
2024-08-30 23:04:56 -0400

    final commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,11 @@ public class Main {
     public static void main(String[] args){
         Car c1 = new Car();
         c1.displayInfo();
+        Vehicle v1 = new Vehicle(6,"blue",3,"diesel");
+        System.out.println(v1.color);
+        System.out.println(v1.numberOfWheels);
+        System.out.println(v1.engineSize);
+        System.out.println(v1.fuelType);
     }
 
 }
